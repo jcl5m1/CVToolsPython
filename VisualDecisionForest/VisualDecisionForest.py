341d16c5ed5302daa73adf199f3b047273de244c
Johnny Lee <jcl5m1@gmail.com>
2014-05-03 13:31:59 -0700

    VisualDecisionForest
    files for creating a visual decision tree classifier

---
+++ VisualDecisionForest/VisualDecisionForest.py
@@ -0,0 +1,139 @@
+__author__ = 'johnny'
+
+import numpy as np
+import cv2
+from itertools import *
+from random import *
+from math import *
+import collections
+
+def generate_random_pixel_order(img, margin):
+    pixel_order = {}
+    for c in range(margin,img.shape[0]-margin):
+        for r in range(margin,img.shape[1]-margin):
+            pixel_order[uniform(0.0,1.0)] = [c,r]
+    od = collections.OrderedDict(sorted(pixel_order.items()))
+    pixel_array = []
+    for k, v in od.iteritems():
+        pixel_array.append(v)
+    return pixel_array
+
+def cross_hair(img, p, size, color, thickness = 1):
+    p1 = (p[0], p[1] + size)
+    p2 = (p[0], p[1] - size)
+    cv2.line(img, p1,p2, color, thickness)
+    p1 = (p[0] + size, p[1])
+    p2 = (p[0] - size, p[1])
+    cv2.line(img, p1,p2, color, thickness)
+
+class TrainingRegion:
+    p1 = (0,0)
+    p2 = (0,0)
+    center = (0,0)
+
+    def __init__(self):
+        self.set((0, 0), (0, 0))
+
+    def set_p2(self, p2):
+        self.set(self.p1, p2)
+
+    def set(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        self.center = ((p1[0] +p2[0])/2, (p1[1] +p2[1])/2)
+
+    def is_equal(self, region):
+        if cmp(self.p1, region.p1) != 0:
+            return False
+        if cmp(self.p2, region.p2) != 0:
+            return False
+        return True
+
+    def offset(self, x, y):
+        self.set((self.p1[0] + x, self.p1[1] + y),(self.p2[0] + x, self.p2[1] + y))
+
+    def width(self):
+        return int(fabs(self.p1[0]-self.p2[0]))
+
+    def height(self):
+        return int(fabs(self.p1[1]-self.p2[1]))
+
+    def draw(self, img, color = (0, 0, 255)):
+        cross_size = 5
+        thickness = 1
+        cv2.rectangle(img, self.p1, self.p2, color, thickness)
+        cross_hair(img, self.center,cross_size, color, thickness)
+
+class PixelSampleTest:
+    x,y,v = 0, 0, 0
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.v = 0
+
+    def randomize(self, img, region):
+        self.x = randint(-region.width()/2, region.width()/2-2)
+        self.y = randint(-region.height()/2, region.height()/2-2)
+        self.v = img[region.center[0] + self.x, region.center[1] + self.y]
+
+    def evaluate(self, img, center):
+        return (img[center[0] + self.x, center[1] + self.y] == self.v).all()
+
+class VisualDecisionTree:
+    left, right, test, best_test, data = None, None, None, None, 0
+    def __init__(self):
+        self.left = None
+        self.Right = None
+        self.test = None
+        self.data = 0
+
+    def train(self, img, region, depth = 5):
+        debug_img = img.copy()
+        #select a few negative random regions from the image as training set
+
+        training_size = 20
+        training_set = [(region,1)]
+
+        for n in range(training_size):
+            test_region = TrainingRegion()
+            x = randint(0,img.shape[0] - region.width())
+            y = randint(0,img.shape[1] - region.height())
+            test_region.set((x,y),(x + region.width(), y + region.height()))
+            if not region.is_equal(test_region):
+                training_set.append((test_region,0))
+
+        for t in training_set:
+            if t[1] == 1:
+                t[0].draw(debug_img, (0, 255, 0))
+            if t[1] == 0:
+                t[0].draw(debug_img, (0, 0, 255))
+
+        #randomize pixel test
+        self.test = PixelSampleTest()
+        self.test.randomize(img, region)
+
+        #compute information gain
+        #iterate a for a while for best test
+        return debug_img
+
+    def classifyImg(self, src_img, dst_img, margin):
+        for c in range(margin,src_img.shape[0]-margin):
+            for r in range(margin,src_img.shape[1] - margin):
+                dst_img[c,r] = self.classify(src_img,(c,r))
+
+    def classifyImgRandomSubsample(self, src_img, dst_img, margin, order, start, count):
+        stop = start+count
+        if stop >= len(order):
+            stop = len(order)-1
+        for n in range(start, stop):
+            c,r = order[n]
+            dst_img[c,r] = self.classify(src_img,(c,r))
+
+    def classify(self, img, p):
+        if self.test == None:
+            return randint(0,255)
+        else:
+            if self.test.evaluate(img, p):
+                return 255
+            else:
+                return 0
---
+++ VisualDecisionForest/VisualDecisionForestTest.py
@@ -0,0 +1,75 @@
+__author__ = 'johnny'
+
+import numpy as np
+import cv2
+from itertools import *
+from random import *
+from VisualDecisionForest import *
+import time
+
+drawing = False # true if mouse is pressed
+mode = True # if True, draw rectangle. Press 'm' to toggle to curve
+ix,iy = -1,-1
+
+tr = TrainingRegion()
+
+# mouse callback function
+def mouse_callback(event,x,y,flags,param):
+    global tr,ix,iy,drawing,mode, src_img, img
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        tr.set((x, y),(x, y))
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing:
+            tr.set_p2((x, y))
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+
+cv2.namedWindow("image", cv2.CV_WINDOW_AUTOSIZE)
+cv2.namedWindow("debug", cv2.CV_WINDOW_AUTOSIZE)
+cv2.namedWindow("class", cv2.CV_WINDOW_AUTOSIZE)
+
+src_img = cv2.imread('..//data//2048-screenshot.png')
+img = src_img.copy()
+class_img = np.zeros((img.shape[0],img.shape[1],1), np.uint8)
+
+tree = VisualDecisionTree()
+
+tr.set((20, 20), (120,120))
+tr.draw(img)
+
+debug_img = tree.train(src_img, tr)
+
+cv2.moveWindow("image", 0,0)
+cv2.moveWindow("debug", src_img.shape[0],0)
+cv2.moveWindow("class", src_img.shape[0]*2,0)
+
+cv2.setMouseCallback('image',mouse_callback)
+
+randomized_pixel_order = generate_random_pixel_order(src_img, tr.width()/2)
+
+pixel_index = 1
+pixels_per_loop = 5000
+
+while(1):
+    if drawing:
+        img = src_img.copy()
+        tr.draw(img)
+    if(pixel_index < len(randomized_pixel_order)):
+        tree.classifyImgRandomSubsample(src_img, class_img, tr.width()/2, randomized_pixel_order, pixel_index, pixels_per_loop)
+        pixel_index += pixels_per_loop
+    cv2.imshow('image', img)
+    cv2.imshow('class', class_img)
+    cv2.imshow("debug", debug_img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord(' '):
+        debug_img = tree.train(src_img, tr)
+        class_img.fill(0)
+        pixel_index = 0
+    elif k == 27:
+        break
+
+cv2.destroyAllWindows()
