5ce21cc3be129e00c181b43ecd02849fd4a3be70
Johnny Lee <jcl5m1@gmail.com>
2014-07-06 00:36:18 -0700

    improved grid angle and length detection

--- PythonCameraCalibration/GridTest.py
+++ PythonCameraCalibration/GridTest.py
@@ -5,6 +5,23 @@ import math
 import matplotlib.pyplot as plt
 import sys
 from matplotlib import animation
+import matplotlib
+from matplotlib import collections  as mc
+
+import matplotlib.animation as animation
+
+def mean_var(data):
+    mean = np.mean(data)
+    c = data-mean
+    var = np.dot(c,c)/data.size
+    return mean, var
+
+def angular_mean_var(data):
+    cosx = [math.cos(x) for x in data]
+    sinx = [math.sin(x) for x in data]
+    mean = math.atan2(np.mean(sinx), np.mean(cosx))
+    return mean
+
 
 def closest_point(points, index):
     closest_dist = sys.float_info.max
@@ -19,14 +36,20 @@ def closest_point(points, index):
     return closest_id, closest_dist
 
 
-def create_grid(n, noise):
-    points = np.zeros((n*n, 2))
+def create_grid(n, noise, outliers):
+    points = np.zeros((n*n + outliers, 2))
     index = 0
     for r in range(-n/2, n/2):
         for c in range(-n/2, n/2):
             points[index, 0] = c+noise*(random.random()-0.5)
             points[index, 1] = r+noise*(random.random()-0.5)
             index += 1
+
+    for i in range(outliers):
+        points[index, 0] = 2*n*(random.random()-0.5)
+        points[index, 1] = 2*n*(random.random()-0.5)
+        index += 1
+
     return points
 
 
@@ -34,97 +57,16 @@ def rotate(points, angle):
     r = np.array([math.cos(angle), -math.sin(angle), math.sin(angle), math.cos(angle)]).reshape(2, 2)
     return points.dot(r)
 
-"""
-
-
-#create a noisy grid of points
-fig_height = 7
-fig = plt.figure(num=None, figsize=(2*fig_height, fig_height), dpi=80, facecolor='w', edgecolor='k')
-leftfig = fig.add_subplot(1, 2, 1)
-pts = leftfig.plot([], [], 'r')
-
-
-
-pts = create_grid(15, .25)
-
-angles = np.zeros((len(pts)))
-centroids = scipy.cluster.vq.kmeans(angles, 2)[0]
-idx = scipy.cluster.vq.vq(angles, centroids)[0]
-plt.subplot(1, 2, 1)
-plt.subplot(1, 2, 1)
-
-
-def init():
-    return
-
-
-def animate(i):
-    global centroids, idx, pts
-    pts = rotate(pts, (math.pi * i) / 60)
-    return
-
-    plt.scatter(pts[:, 0], pts[:, 1])
-    b = np.mat('1;1')
-    for id1 in range(len(pts)):
-        id2 = closest_point(pts, id1)[0]
-        plt.subplot(1, 2, 1)
-        plt.plot([pts[id1, 0], pts[id2, 0]], [pts[id1, 1], pts[id2, 1]], 'r')
-
-        A = np.mat(np.concatenate((pts[id1], pts[id2])).reshape(2, 2))
-        x = np.linalg.solve(A, b)
-
-        a = math.atan(-x[0]/x[1])
-        angles[id1] = a
-    centroids = scipy.cluster.vq.kmeans(angles, 2)[0]
-    idx = scipy.cluster.vq.vq(angles, centroids)[0]
-    plt.subplot(1, 2, 2)
-    plt.plot(angles[idx == 0], 'ob', angles[idx == 1], 'or')
-    plt.plot(centroids[:], centroids[:], 'sg', markersize=8)
-
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=20, blit=True)
-
-
-
-
-# First set up the figure, the axis, and the plot element we want to animate
-#fig = plt.figure()
-#ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
-#line, = ax.plot([], [], lw=2)
-
-
-# initialization function: plot the background of each frame
-#def init():
-#    line.set_data([], [])
-#    return line,
-
-
-# animation function.  This is called sequentially
-#def animate(i):
-#    x = np.linspace(0, 2, 1000)
-#    y = np.sin(2 * np.pi * (x - 0.01 * i))
-#    line.set_data(x, y)
-#    return line,
-
-
-# save the animation as an mp4.  This requires ffmpeg or mencoder to be
-# installed.  The extra_args ensure that the x264 codec is used, so that
-# the video can be embedded in html5.  You may need to adjust this for
-# your system: for more information, see
-# http://matplotlib.sourceforge.net/api/animation_api.html
-#anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
-
-#plt.show()
-"""
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 
 class Grid:
-    def __init__(self, dim=10, noise=0.05):
-        self.points = create_grid(dim, noise)
+    def __init__(self, dim=10, noise=0.1, outliers=0):
+        self.points = create_grid(dim, noise, outliers)
+        self.angles = np.zeros(len(self.points))
+        self.lengths = np.zeros(len(self.points))
+        self.angle = []
+        self.length = []
+        self.linedata = np.zeros((3*len(self.points), 2))
 
     def step(self, rotation=0):
         self.points = rotate(self.points, rotation)
@@ -132,45 +74,77 @@ class Grid:
     def analyze(self):
         for id1 in range(len(self.points)):
             id2 = closest_point(self.points, id1)[0]
-#            plt.subplot(1, 2, 1)
-#            plt.plot([pts[id1, 0], pts[id2, 0]], [pts[id1, 1], pts[id2, 1]], 'r')
+            self.linedata[3*id1] = self.points[id1]
+            self.linedata[3*id1+1] = self.points[id2]
+            self.linedata[3*id1+2] = [None, None]
+            a = math.atan2((self.points[id1, 1] - self.points[id2, 1]),(self.points[id1,0] - self.points[id2,0]))
+            self.angles[id1] = 4*a # because we are repeating every pi/2, compress the angle space by 4x
+            self.lengths[id1] = np.linalg.norm(self.points[id1] - self.points[id2])
+        self.angle = angular_mean_var(self.angles)
+        self.length = mean_var(self.lengths)
+
+
+grid_dim = 10
+grid_noise = 0.1
+grid_outliers = 10
+grid = Grid(grid_dim, grid_noise, grid_outliers)
 
 
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
 
-scale = 10
-ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(-scale, scale), ylim=(-scale, scale))
-data, = ax.plot([], [], 'ro')
-line, = ax.plot([], [], 'bo-')
-grid = Grid(10, 0.3)
+scale = 20
+gridplot = fig.add_subplot(141, aspect='equal', autoscale_on=False, xlim=(-scale, scale), ylim=(-scale, scale))
+angleplot = fig.add_subplot(142, aspect='equal', autoscale_on=False, xlim=(-1.2,1.2), ylim=(-1.2,1.2))
+lengthplot = fig.add_subplot(143, aspect='equal', autoscale_on=False, xlim=(-.5,.5), ylim=(0,3))
+gridplot2 = fig.add_subplot(144, aspect='equal', autoscale_on=False, xlim=(-scale, scale), ylim=(-scale, scale))
+#angleplot = fig.add_subplot(122, autoscale_on=False, xlim=(-.5, 0.5), ylim=(-14.2, 14.2) )
+
+
+line, = gridplot.plot([])
+data, = gridplot.plot([], [], 'ro')
+
+angle_data, = angleplot.plot([],[], 'ro')
+mean_angle_data = plt.Circle((0, 0), 0.25, fc='c')
+angleplot.add_patch(mean_angle_data)
+
+length_data, = lengthplot.plot([],[], 'ro')
+mean_length_data = plt.Circle((0, 0), 0.25, fc='c')
+lengthplot.add_patch(mean_length_data)
+
+data2, = gridplot2.plot([], [], 'ro')
 
 # initialization function: plot the background of each frame
 def init():
     data.set_data([], [])
-    return data, line
+    return
 
 # animation function.  This is called sequentially
 def animate(i):
-    global grid, ax
-    grid.points = create_grid(10, .3)
+    global grid, gridplot
+    grid.points = create_grid(grid_dim, grid_noise, grid_outliers)
     grid.step(i/30.0)
-  #  ax.plot([0, 5], [0, 5], 'o-')
-    data.set_data(grid.points[:, 0], grid.points[:, 1])
-#    line.set_data([0, 5], [0, 5])
-    line.set_data([0, 5], [0, 5+math.sin(i/10.0)])
+    grid.analyze()
 
-    return data, line
+    data.set_data(grid.points[:, 0], grid.points[:, 1])
+#    line.set_data(grid.linedata[:, 0], grid.linedata[:, 1])
+
+    mean_angle_data.center = (math.cos(grid.angle), math.sin(grid.angle))
+    angle_data.set_data([math.cos(x) for x in grid.angles], [math.sin(x) for x in grid.angles])
+
+    length_data.set_data([0], grid.lengths)
+    mean_length_data.center = (0,grid.length[0])
+    mean_length_data.radius = math.sqrt(grid.length[1])
+
+
+    rotated_points = rotate(grid.points, grid.angle/4)
+    data2.set_data(rotated_points[:,0], rotated_points[:,1])
+
+    return
+
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=20000, interval=20, blit=True)
-
-# save the animation as an mp4.  This requires ffmpeg or mencoder to be
-# installed.  The extra_args ensure that the x264 codec is used, so that
-# the video can be embedded in html5.  You may need to adjust this for
-# your system: for more information, see
-# http://matplotlib.sourceforge.net/api/animation_api.html
-#anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+                               frames=20000, interval=20, blit=False)
 
 plt.show()
