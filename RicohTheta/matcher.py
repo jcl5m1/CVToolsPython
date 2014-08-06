7e0334ebc71ee62d6af236471fb2cfc6e1b614bb
Johnny Lee <jcl5m1@gmail.com>
2014-08-06 02:30:45 -0700

    improved pinhole dewarping

--- RicohTheta/matcher.py
+++ RicohTheta/matcher.py
@@ -4,6 +4,9 @@ import numpy as np
 import math
 import os
 import argparse
+import sys
+sys.path.append('../common/')
+import transformations
 
 
 def pinhole_from_cylindrical_dewarp(src_img, dst_img, src_center, dst_fov):
@@ -12,20 +15,32 @@ def pinhole_from_cylindrical_dewarp(src_img, dst_img, src_center, dst_fov):
 
     src_f = float(src_h)/math.pi
 
-    print math.tan(dst_fov/2.0)
+    rot = transformations.euler_matrix(src_center[1],src_center[0],0)
 
-    q = math.tan(dst_fov/2.0)*2.0*(0-dst_w/2.0)/float(dst_w)
-    q2 = math.tan(dst_fov/2.0)*2.0*(200000-dst_w/2.0)/float(dst_w)
-    print "FOV", q, math.atan(q)
-    print "FOV", q2, math.atan(q2)
+    pixel_scaler = math.tan(dst_fov/2.0)*2.0/float(dst_w)
 
     for r in range(dst_h):
         for c in range(dst_w):
-            ax = math.atan(math.tan(dst_fov/2.0)*2.0*(c-dst_w/2.0)/float(dst_w))
-            ay = math.atan(math.tan(dst_fov/2.0)*2.0*(r-dst_h/2.0)/float(dst_w))
 
-            src_x = src_f*ax+src_center[0]
-            src_y = src_f*ay+src_center[1]
+            dx = pixel_scaler*(c-dst_w/2.0)
+            dy = pixel_scaler*(r-dst_h/2.0)
+
+            azimuth = math.atan(dx)
+            altitude = math.atan(dy/math.sqrt(dx*dx+1))
+
+            y = math.sin(altitude)
+            d = math.sqrt(1-y*y)
+            x = math.sin(azimuth)*d
+            z = math.cos(azimuth)*d
+            p = [x, y, z, 1]
+            p2 = rot.dot(p)
+
+
+            altitude = math.asin(p2[1])
+            azimuth = math.atan2(p2[0],p2[2])
+
+            src_x = src_f*azimuth + src_img.shape[1]/2
+            src_y = src_f*altitude + src_img.shape[0]/2
 
             if src_x < 0:
                 continue
@@ -41,8 +56,7 @@ def pinhole_from_cylindrical_dewarp(src_img, dst_img, src_center, dst_fov):
 
 def drawMatches(img1, kp1, img2, kp2, matches, mask):
     color = (0,255,0)
-    result_img = np.zeros((img1.shape[1], img1.shape[0] + img2.shape[0], 3), np.uint8)
-
+    result_img = np.zeros((img1.shape[0]+img2.shape[0], img1.shape[1], 3), np.uint8)
     result_img[0:img1.shape[0], 0:img1.shape[1]] = img1
     result_img[img1.shape[0]:(img1.shape[0]+img2.shape[0]), 0:img2.shape[1]] = img2
 
@@ -58,52 +72,59 @@ def drawMatches(img1, kp1, img2, kp2, matches, mask):
     return result_img
 
 
+def ExtractAndMatch(img1,img2):
+    orb = cv2.ORB(2000, 1.2, 8, 31)
+
+    kp1, des1 = orb.detectAndCompute(img1, None)
+    kp2, des2 = orb.detectAndCompute(img2, None)
+
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    # Match descriptors.
+    matches = bf.match(des1, des2)
+
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key = lambda x:x.distance)
+
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 10.0)
+
+    return drawMatches(img1, kp1, img2, kp2, matches, mask)
+
+
+
 file1 = "./data/R0010001.JPG"
 file2 = "./data/R0010002.JPG"
 
 print file1
 print file2
 
-img1 = cv2.imread(file1)
-img2 = cv2.imread(file2)
+img1_big = cv2.imread(file1)
+img2_big = cv2.imread(file2)
 
 downsample = 4
 
-img1_size = img1.shape
-img2_size = img1.shape
-img1_small = cv2.resize(img1, (img1_size[1]/downsample, img1_size[0]/downsample))
-img2_small = cv2.resize(img2, (img2_size[1]/downsample, img2_size[0]/downsample))
+img1_size = img1_big.shape
+img2_size = img1_big.shape
+img1_small = cv2.resize(img1_big, (img1_size[1]/downsample, img1_size[0]/downsample))
+img2_small = cv2.resize(img2_big, (img2_size[1]/downsample, img2_size[0]/downsample))
 
-orb = cv2.ORB(2000, 1.2, 8, 31)
 
-kp1, des1 = orb.detectAndCompute(img1_small, None)
-kp2, des2 = orb.detectAndCompute(img2_small, None)
+dewarp_size = 400
+dewarp_img1 = np.zeros((dewarp_size, dewarp_size, 3), np.uint8)
+pinhole_from_cylindrical_dewarp(img1_small, dewarp_img1, (0,0), 2.0)
+dewarp_img2 = np.zeros((dewarp_size, dewarp_size, 3), np.uint8)
+pinhole_from_cylindrical_dewarp(img2_small, dewarp_img2, (-.20,0), 2.0)
 
-# create BFMatcher object
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-# Match descriptors.
-matches = bf.match(des1, des2)
-
-# Sort them in the order of their distance.
-matches = sorted(matches, key = lambda x:x.distance)
-
-src_pts = np.float32([ kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-dst_pts = np.float32([ kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
-M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 10.0)
-matchesMask = mask.ravel().tolist()
-
-img3 = drawMatches(img1_small, kp1, img2_small, kp2, matches, mask)
-
-dewarp_img = np.zeros((500, 500, 3), np.uint8)
-
-print img2_small.shape[1]/2, img2_small.shape[0]/2
-pinhole_from_cylindrical_dewarp(img1_small, dewarp_img, (img1_small.shape[1]/2, img1_small.shape[0]/2), 2.0)
+#img3 = ExtractAndMatch(img1_small, img2_small)
+img3 = ExtractAndMatch(dewarp_img1,dewarp_img2)
 
 cv2.imshow("img3", img3)
 cv2.moveWindow("img3", 0, 0)
-
-cv2.imshow("dewarp", dewarp_img)
+#cv2.imshow("dewarp", dewarp_img1)
 
 
 cv2.waitKey()
