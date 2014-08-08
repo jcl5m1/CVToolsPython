56fb14bf3660141db753d8b0fe12f24ca0a4b6f6
Johnny Lee <jcl5m1@gmail.com>
2014-08-07 21:42:57 -0700

    LK rotation code

---
+++ LKFlowRotation/flow_rotation.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+
+'''
+Lucas-Kanade tracker
+====================
+
+Lucas-Kanade sparse optical flow demo. Uses goodFeaturesToTrack
+for track initialization and back-tracking for match verification
+between frames.
+
+Usage
+-----
+lk_track.py [<video_source>]
+
+
+Keys
+----
+ESC - exit
+'''
+
+import cv2
+import numpy as np
+import math
+
+def procrustes(X, Y, scaling=True, reflection='best'):
+    """
+    A port of MATLAB's `procrustes` function to Numpy.
+
+    Procrustes analysis determines a linear transformation (translation,
+    reflection, orthogonal rotation and scaling) of the points in Y to best
+    conform them to the points in matrix X, using the sum of squared errors
+    as the goodness of fit criterion.
+
+        d, Z, [tform] = procrustes(X, Y)
+
+    Inputs:
+    ------------
+    X, Y
+        matrices of target and input coordinates. they must have equal
+        numbers of  points (rows), but Y may have fewer dimensions
+        (columns) than X.
+
+    scaling
+        if False, the scaling component of the transformation is forced
+        to 1
+
+    reflection
+        if 'best' (default), the transformation solution may or may not
+        include a reflection component, depending on which fits the data
+        best. setting reflection to True or False forces a solution with
+        reflection or no reflection respectively.
+
+    Outputs
+    ------------
+    d
+        the residual sum of squared errors, normalized according to a
+        measure of the scale of X, ((X - X.mean(0))**2).sum()
+
+    Z
+        the matrix of transformed Y-values
+
+    tform
+        a dict specifying the rotation, translation and scaling that
+        maps X --> Y
+
+    """
+
+    n,m = X.shape
+    ny,my = Y.shape
+
+    muX = X.mean(0)
+    muY = Y.mean(0)
+
+    X0 = X - muX
+    Y0 = Y - muY
+
+    ssX = (X0**2.).sum()
+    ssY = (Y0**2.).sum()
+
+    # centred Frobenius norm
+    normX = np.sqrt(ssX)
+    normY = np.sqrt(ssY)
+
+    # scale to equal (unit) norm
+    X0 /= normX
+    Y0 /= normY
+
+    if my < m:
+        Y0 = np.concatenate((Y0, np.zeros(n, m-my)),0)
+
+    # optimum rotation matrix of Y
+    A = np.dot(X0.T, Y0)
+    U,s,Vt = np.linalg.svd(A,full_matrices=False)
+    V = Vt.T
+    T = np.dot(V, U.T)
+
+    if reflection is not 'best':
+
+        # does the current solution use a reflection?
+        have_reflection = np.linalg.det(T) < 0
+
+        # if that's not what was specified, force another reflection
+        if reflection != have_reflection:
+            V[:,-1] *= -1
+            s[-1] *= -1
+            T = np.dot(V, U.T)
+
+    traceTA = s.sum()
+
+    if scaling:
+
+        # optimum scaling of Y
+        b = traceTA * normX / normY
+
+        # standarised distance between X and b*Y*T + c
+        d = 1 - traceTA**2
+
+        # transformed coords
+        Z = normX*traceTA*np.dot(Y0, T) + muX
+
+    else:
+        b = 1
+        d = 1 + ssY/ssX - 2 * traceTA * normY / normX
+        Z = normY*np.dot(Y0, T) + muX
+
+    # transformation matrix
+    if my < m:
+        T = T[:my,:]
+    c = muX - b*np.dot(muY, T)
+
+    #transformation values
+    tform = {'rotation':T, 'scale':b, 'translation':c}
+
+    return d, Z, tform
+
+lk_params = dict( winSize  = (15, 15),
+                  maxLevel = 2,
+                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+feature_params = dict( maxCorners = 500,
+                       qualityLevel = 0.3,
+                       minDistance = 7,
+                       blockSize = 7 )
+
+cap = cv2.VideoCapture(1)
+
+cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640)
+cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,480)
+
+track_len = 10
+rotation_history_len = 300
+detect_interval = 5
+tracks = []
+rotation_track = []
+frame_idx = 0
+prev_gray = 0
+total_rot = 0
+while True:
+    ret,frame = cap.read()
+    cv2.imshow('e2',frame)
+
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    vis = frame.copy()
+
+    if len(tracks) > 0:
+        img0, img1 = prev_gray, frame_gray
+        p0 = np.float32([tr[-1] for tr in tracks]).reshape(-1, 1, 2)
+        p1, st, err = cv2.calcOpticalFlowPyrLK(img0, img1, p0, None, **lk_params)
+        p0r, st, err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **lk_params)
+        d = abs(p0-p0r).reshape(-1, 2).max(-1)
+        good = d < 1
+        new_tracks = []
+        curr_pts = []
+        prev_pts = []
+        for tr, (x, y), good_flag in zip(tracks, p1.reshape(-1, 2), good):
+            if not good_flag:
+                continue
+            tr.append((x, y))
+            curr_pts.append(tr[len(tr)-1])
+            prev_pts.append(tr[len(tr)-2])
+            if len(tr) > track_len:
+                del tr[0]
+            new_tracks.append(tr)
+            cv2.circle(vis, (x, y), 2, (0, 255, 0), -1)
+        tracks = new_tracks
+        cv2.polylines(vis, [np.int32(tr) for tr in tracks], False, (0, 255, 0))
+        if len(curr_pts) > 4:
+            d,Z, tform = procrustes(np.array(prev_pts), np.array(curr_pts))
+            rot = math.atan2(tform['rotation'][0,1] , tform['rotation'][0,0])
+            total_rot += rot
+
+            #print rotation rate to console
+            print rot
+
+            #create a history for plotting
+            rotation_track.append(rot)
+            if len(rotation_track) > rotation_history_len:
+                del rotation_track[0]
+
+        #plot rotation
+        center = (vis.shape[1]/2,vis.shape[0]/2)
+
+
+
+        x = 0
+        x_step = vis.shape[1]/rotation_history_len
+        rot_scale = 200
+        prev_rot = 0
+        #plot rotation line
+        cv2.line(vis,center, (center[0] + int(rot_scale*math.cos(-total_rot)),center[1] + int(rot_scale*math.sin(-total_rot))),(255,0,0))
+        #plot rotation history
+        for rot in rotation_track:
+            cv2.line(vis,(x,(int(prev_rot*rot_scale) + vis.shape[0]/2)), (x+x_step,int(rot*rot_scale) + vis.shape[0]/2),(0,0,255))
+            prev_rot = rot
+            x += x_step
+
+
+    if frame_idx % detect_interval == 0:
+        mask = np.zeros_like(frame_gray)
+        mask[:] = 255
+        for x, y in [np.int32(tr[-1]) for tr in tracks]:
+            cv2.circle(mask, (x, y), 5, 0, -1)
+        p = cv2.goodFeaturesToTrack(frame_gray, mask = mask, **feature_params)
+        if p is not None:
+            for x, y in np.float32(p).reshape(-1, 2):
+                tracks.append([(x, y)])
+
+    frame_idx += 1
+    prev_gray = frame_gray
+    cv2.imshow('lk_track', vis)
+
+    ch = cv2.waitKey(1)
+    if ch == 27:
+        break
+    if ch == ord('r'):
+        total_rot = 0
